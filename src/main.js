93ebc64bf159df8b906c9abc4e7fbfe84e038408
Rodrigo Pacheco <rodrigopacheco100@gmail.com>
2020-06-16 11:03:20 -0300

    .

--- src/main.js
+++ src/main.js
@@ -1,1 +1,27 @@
-alert('teste2')
+class App {
+    constructor() {
+        this.repositories = []
+
+        this.formEl = document.querySelector('#repo-form')
+
+        this.registerHandlers()
+    }
+
+    registerHandlers() {
+        this.formEl.onsubmit = event => this.addRepository(event);
+    }
+
+    addRepository(event) {
+        event.preventDefault()
+
+        this.repositories.push({
+            name: 'rocketseat.com.br',
+            description: 'Tire a sua ideia do papel e dê vida à sua startup.',
+            avatar_url: 'https://avatars0.githubusercontent.com/u/28929274?v=4',
+            html_url: 'https://github.com/rocketseat',
+        })
+        console.log(this.repositories)
+    }
+}
+
+new App()
